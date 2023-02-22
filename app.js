f53b023d7d68e7ea3e34ef40d6a489e928fa7371
Izac <izikki11499@gmail.com>
2023-02-21 20:16:44 -0800

    General gameplay complete, revisions on playRound() Had switched console logs to return statements in last commit( or somethin), after testing: not desired outcome, so switched back to logs, but general gameplay loop is built!

--- app.js
+++ app.js
@@ -1,57 +1,105 @@
-console.log("Hello");//Saying hello
+console.log("Hello"); //Saying hello
 
 //Array of possible answers
-const choices = ['Rock', 'Paper', 'Scissors'];
+const choices = ["Rock", "Paper", "Scissors"];
 //Scores
 let playerScore = 0;
 let computerScore = 0;
 
 //Players(users)choice
 const playerChoice = () => {
-    let answer = prompt("Pick Rock, paper, or scissors:");
-    return answer;
-}
+  let answer = prompt("Pick Rock, paper, or scissors:");
+  return answer;
+};
 //Computers choice
 const computerChoice = () => {
-    let randomNumber = Math.floor(Math.random() * (0 + 3));
-    return choices[randomNumber];
-}
+  let randomNumber = Math.floor(Math.random() * (0 + 3));
+  return choices[randomNumber];
+};
 //Round of play
 const playRound = (playerChoice, computerChoice) => {
-    //Player picked Rock?
-    if(playerChoice.toUpperCase() === 'rock'.toUpperCase() && computerChoice.toUpperCase() === 'paper'.toUpperCase()){
-        computerScore++;
-        return `Computer's ${computerChoice} beats players ${playerChoice}, +1 point to Computer`;
-    }else if(playerChoice.toUpperCase() == 'rock'.toUpperCase() && computerChoice.toUpperCase() === 'scissors'.toUpperCase()){
-        playerScore++;
-        return `Player's ${playerChoice} beats computers ${computerChoice}, +1 point to Player`;
-    }else if(playerChoice.toUpperCase() == 'rock'.toUpperCase() && computerChoice.toUpperCase() === 'rock'.toUpperCase()){
-        return `Player and Computer picked ${playerChoice}`;
-    }
-    //Player picked Scissors?
-    else if(playerChoice.toUpperCase() == 'scissors'.toUpperCase() && computerChoice.toUpperCase() === 'rock'.toUpperCase()){
-        computerScore++;
-        return`Computer's ${computerChoice} beats players ${playerChoice}, +1 point to Computer`;
-    }
-    else if(playerChoice.toUpperCase() == 'scissors'.toUpperCase() && computerChoice.toUpperCase() === 'paper'.toUpperCase()){
-        playerScore++;
-        return `Player's ${playerChoice} beats computers ${computerChoice}, +1 point to Player`;
-    }
-    else if(playerChoice.toUpperCase() == 'scissors'.toUpperCase() && computerChoice.toUpperCase() === 'scissors'.toUpperCase()){
-        return `Player and Computer picked ${playerChoice}`;
-    }
-    //Player picked Paper
-    else if(playerChoice.toUpperCase() == 'paper'.toUpperCase() && computerChoice.toUpperCase() === 'scissors'.toUpperCase()){
-        computerScore++;
-        return `Computer's ${computerChoice} beats players ${playerChoice}, +1 point to Computer`;
-    }
-    else if(playerChoice.toUpperCase() == 'paper'.toUpperCase() && computerChoice.toUpperCase() === 'rock'.toUpperCase()){
-        playerScore++;
-        return `Player's ${playerChoice} beats computers ${computerChoice}, +1 point to Player`;
-    }
-    else if(playerChoice.toUpperCase() == 'scissors'.toUpperCase() && computerChoice.toUpperCase() === 'scissors'.toUpperCase()){
-        return `Player and Computer picked ${playerChoice}`;
-    }
-
-}
+  //Player picked Rock?
+  if (
+    playerChoice.toUpperCase() === "rock".toUpperCase() &&
+    computerChoice.toUpperCase() === "paper".toUpperCase()
+  ) {
+    computerScore++;
+    console.log(
+      `Computer's ${computerChoice} beats players ${playerChoice}, +1 point to Computer`
+    );
+  } else if (
+    playerChoice.toUpperCase() == "rock".toUpperCase() &&
+    computerChoice.toUpperCase() === "scissors".toUpperCase()
+  ) {
+    playerScore++;
+    console.log(
+      `Player's ${playerChoice} beats computers ${computerChoice}, +1 point to Player`
+    );
+  } else if (
+    playerChoice.toUpperCase() == "rock".toUpperCase() &&
+    computerChoice.toUpperCase() === "rock".toUpperCase()
+  ) {
+    console.log(`Player and Computer picked ${playerChoice}`);
+  }
+  //Player picked Scissors?
+  else if (
+    playerChoice.toUpperCase() == "scissors".toUpperCase() &&
+    computerChoice.toUpperCase() === "rock".toUpperCase()
+  ) {
+    computerScore++;
+    console.log(
+      `Computer's ${computerChoice} beats players ${playerChoice}, +1 point to Computer`
+    );
+  } else if (
+    playerChoice.toUpperCase() == "scissors".toUpperCase() &&
+    computerChoice.toUpperCase() === "paper".toUpperCase()
+  ) {
+    playerScore++;
+    console.log(
+      `Player's ${playerChoice} beats computers ${computerChoice}, +1 point to Player`
+    );
+  } else if (
+    playerChoice.toUpperCase() == "scissors".toUpperCase() &&
+    computerChoice.toUpperCase() === "scissors".toUpperCase()
+  ) {
+    console.log(`Player and Computer picked ${playerChoice}`);
+  }
+  //Player picked Paper
+  else if (
+    playerChoice.toUpperCase() == "paper".toUpperCase() &&
+    computerChoice.toUpperCase() === "scissors".toUpperCase()
+  ) {
+    computerScore++;
+    console.log(
+      `Computer's ${computerChoice} beats players ${playerChoice}, +1 point to Computer`
+    );
+  } else if (
+    playerChoice.toUpperCase() == "paper".toUpperCase() &&
+    computerChoice.toUpperCase() === "rock".toUpperCase()
+  ) {
+    playerScore++;
+    console.log(
+      `Player's ${playerChoice} beats computers ${computerChoice}, +1 point to Player`
+    );
+  } else if (
+    playerChoice.toUpperCase() == "scissors".toUpperCase() &&
+    computerChoice.toUpperCase() === "scissors".toUpperCase()
+  ) {
+    console.log(`Player and Computer picked ${playerChoice}`);
+  } else {
+    console.log(`Come again?`);
+  }
+};
 //5 Round game, winner decided at end
+const game = () => {
+  for (let i = 0; i <= 5; i++) {
+    playRound(playerChoice(), computerChoice());
+  }
+  if (playerScore > computerScore) {
+    return `Player has won! Player score: ${playerScore}, Computer score: ${computerScore}`;
+  } else if (computerScore > playerScore) {
+    return `Computer has won!  Player score: ${playerScore}, Computer score: ${computerScore}`;
+  } else {
+    return `Probably a tie?`;
+  }
+};
